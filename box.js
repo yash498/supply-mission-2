55e96d4a1e7792926a3a0ab24bf13dc084104afe
yash498 <71262491+yash498@users.noreply.github.com>
2020-11-02 19:04:47 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,36 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+            
+         'isStatic':true,    
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      
+     fill(255);
+      
+      rect(0, 0, this.width, this.height);
+     
+    }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
